e704a6bf6486c00b1b0324f33d37e56db4c7d923
Braidon <63606230+Treefire33@users.noreply.github.com>
2022-05-26 22:37:11 -0500

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,74 @@
+var money = 20000000;
+var moneyStr = "20,000,000";
+var actionsDoneToday = 0;
+var days = 0;
+
+$("#spend").click(function(){
+    $('.spendChoice').show();
+    randomSpendChoices();
+});
+$('.choice').click(function(){
+    $('.spendChoice').hide();
+});
+
+$("#advance").click(function(){
+  if(actionsDoneToday === 6)
+  {
+    money += getRandomInt(500000, 750000);
+    let val = money.toLocaleString("en-US");
+    moneyStr = val;
+    $("#moneyValue").text(moneyStr);
+    actionsDoneToday = 0;
+    $("#actionsLeft").text("Actions Left Today: " + (6 - actionsDoneToday));
+    days += 1;
+    $("#daysCompleted").text("Days Completed: "+days);
+    let chooser = getRandomInt(0, currentEvents.length-1);
+    $("#event").text(currentEvents[chooser]);
+  }
+  else{
+    alert("Can't advance day!");
+  }
+});
+
+var spendChoiceTypes = ["buying expensive cars", "charity", "cats", "houses", "ice cream", "data", "parking tickets", "unpaid bills"];
+var spendChoiceTypeAmounts = [1700000, 10000, 250, 350000, 10, 5000, 800, 4300];
+
+var spendChoiceAmounts = [100,100,100];
+
+function randomSpendChoices()
+{
+  let chooserNum = getRandomInt(0,spendChoiceTypes.length-1);
+  $("#spendC1").text("Spend on "+spendChoiceTypes[chooserNum]);
+  spendChoiceAmounts[0] = spendChoiceTypeAmounts[chooserNum];
+  
+  let chooserNum2 = getRandomInt(0,spendChoiceTypes.length-1);
+  $("#spendC2").text("Spend on "+spendChoiceTypes[chooserNum2]);
+  spendChoiceAmounts[1] = spendChoiceTypeAmounts[chooserNum2];
+  
+  let chooserNum3 = getRandomInt(0,spendChoiceTypes.length-1);
+  $("#spendC3").text("Spend on "+spendChoiceTypes[chooserNum3]);
+  spendChoiceAmounts[2] = spendChoiceTypeAmounts[chooserNum3];
+}
+
+function spendMoney(spendChoice)
+{
+  if(actionsDoneToday < 6)
+  {
+    money -= spendChoiceAmounts[spendChoice];
+    let val = money.toLocaleString("en-US");
+    moneyStr = val;
+    actionsDoneToday += 1;
+  }
+  $("#moneyValue").text(moneyStr);
+  $("#actionsLeft").text("Actions Left Today: " + (6 - actionsDoneToday));
+}
+
+$("#spendC1").click(function(){spendMoney(0)});
+$("#spendC2").click(function(){spendMoney(1)});
+$("#spendC3").click(function(){spendMoney(2)});
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
---
+++ js/vals.js
@@ -0,0 +1,14 @@
+var currentEvents = [
+  "Random electric company kills bugs for some reason.",
+  "Mart of Walls starts selling electric bikes.",
+  "Tim Adams recieves a $20,000,000 inheritance, tries to spend it all.",
+  "10 ways to heal your skin!              Wait... wrong website.",
+  "Plauge Inc. identifies new virus.",
+  "Was it boredom that killed the cat? Find out here!",
+  "\"Last time I punched a wall, my hand broke!\" says local rager",
+  "Everday I wake up, then I start to break up.",
+  "Woo-ee-oo I look just like buddy holly.",
+  "Bull comments on how targets having a \"bull's eye\" is really harmful to bulls.",
+  "Jessica comes out as lesbian, everyone supports her, as they should.",
+  "Popular influencer #90217321236129 tries to get free pizza, instead is punched so hard they get sent to the moon."
+];
